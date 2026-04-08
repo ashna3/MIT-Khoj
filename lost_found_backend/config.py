f7512e5515b25f798f319884660825036ff2eeea
Ashna <ashna3@users.noreply.github.com>
2026-04-08 08:38:25 +0530

    Sanitize default config secrets

--- lost_found_backend/config.py
+++ lost_found_backend/config.py
@@ -6,6 +6,6 @@ load_dotenv()
 class Config:
     DB_HOST = os.environ.get('DB_HOST', '127.0.0.1')
     DB_USER = os.environ.get('DB_USER', 'root')
-    DB_PASSWORD = os.environ.get('DB_PASSWORD', '&q7(%P1:4')
+    DB_PASSWORD = os.environ.get('DB_PASSWORD', '')
     DB_NAME = os.environ.get('DB_NAME', 'lost_found_db')
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'super-secret-lost-found-key')
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'change-me-in-env')
